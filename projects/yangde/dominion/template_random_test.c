0415484d65f35159181614efcf265f032ceaa3a1
Derek <yangd01234567@gmail.com>
2019-02-22 13:47:18 -0800

    Added code for template_random_test.c

--- projects/yangde/dominion/template_random_test.c
+++ projects/yangde/dominion/template_random_test.c
@@ -1,3 +1,9 @@
+/*Derek Yang
+ * Random Testing Quiz
+ *
+ *
+ */
+
 #include<stdio.h>
 #include<string.h>
 #include<stdlib.h>
@@ -5,47 +11,55 @@
 
 char inputChar()
 {
-    // TODO: rewrite this function
-    return ' ';
+    return ((char)(rand() % (94)) + 32);
 }
 
 char *inputString()
 {
-    // TODO: rewrite this function
-    return "";
+    int i;
+    char str[5];
+    char* test_str = str;
+    for (i = 0; i < 5; i++)
+    {
+        str[i] = (char)rand() % (16) + 101;
+    }
+    //set null terminating value
+    str[5] = '\0';
+    return test_str;
 }
+
 
 void testme()
 {
-  int tcCount = 0;
-  char *s;
-  char c;
-  int state = 0;
-  while (1)
-  {
-    tcCount++;
-    c = inputChar();
-    s = inputString();
-    printf("Iteration %d: c = %c, s = %s, state = %d\n", tcCount, c, s, state);
-
-    if (c == '[' && state == 0) state = 1;
-    if (c == '(' && state == 1) state = 2;
-    if (c == '{' && state == 2) state = 3;
-    if (c == ' '&& state == 3) state = 4;
-    if (c == 'a' && state == 4) state = 5;
-    if (c == 'x' && state == 5) state = 6;
-    if (c == '}' && state == 6) state = 7;
-    if (c == ')' && state == 7) state = 8;
-    if (c == ']' && state == 8) state = 9;
-    if (s[0] == 'r' && s[1] == 'e'
-       && s[2] == 's' && s[3] == 'e'
-       && s[4] == 't' && s[5] == '\0'
-       && state == 9)
+    int tcCount = 0;
+    char *s;
+    char c;
+    int state = 0;
+    while (1)
     {
-      printf("error ");
-      exit(200);
+        tcCount++;
+        c = inputChar();
+        s = inputString();
+        printf("Iteration %d: c = %c, s = %s, state = %d\n", tcCount, c, s, state);
+
+        if (c == '[' && state == 0) state = 1;
+        if (c == '(' && state == 1) state = 2;
+        if (c == '{' && state == 2) state = 3;
+        if (c == ' '&& state == 3) state = 4;
+        if (c == 'a' && state == 4) state = 5;
+        if (c == 'x' && state == 5) state = 6;
+        if (c == '}' && state == 6) state = 7;
+        if (c == ')' && state == 7) state = 8;
+        if (c == ']' && state == 8) state = 9;
+        if (s[0] == 'r' && s[1] == 'e'
+            && s[2] == 's' && s[3] == 'e'
+            && s[4] == 't' && s[5] == '\0'
+            && state == 9)
+        {
+            printf("error ");
+            exit(200);
+        }
     }
-  }
 }
 
 
